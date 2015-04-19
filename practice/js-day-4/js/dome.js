c2a9e81b0cb91ffe058accce3ddd0f04c1978c8b
金伟强 <iamjoel007@gmail.com>
2015-04-19 21:50:12 +0800

    fix document.body null

--- practice/js-day-4/js/dome.js
+++ practice/js-day-4/js/dome.js
@@ -1,6 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+	addHtml();
+});
 function addHtml() {
 	var a=document.createElement("p");
 	a.innerHTML="随你走在天际，看繁华满地。<span>！！！</span>";
 	document.body.appendChild(a);
 }
-	addHtml();
